890e2d60230a09c3ad5061711dfb29561123d7b0
Shubham <gaur3786@gmail.com>
2026-03-26 15:09:20 +0530

    data change version 2

--- mycode.py
+++ mycode.py
@@ -2,9 +2,9 @@ import pandas as pd
 import os
 
 # Create a sample DataFrame with column names
-data = {'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 35],
-    'City': ['New York', 'Los Angeles', 'Chicago']
+data = {'Name': ['Alice', 'Bob', 'Charlie','David'],
+    'Age': [25, 30, 35, 40],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
     }
 
 df = pd.DataFrame(data)
